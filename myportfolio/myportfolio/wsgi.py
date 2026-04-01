930dfd60800b660267429802af9b74455be41876
Maricar Pordan <pordanmaricar26@gmail.com>
2026-04-01 09:30:21 -0800

    Update wsgi configuration

--- myportfolio/myportfolio/wsgi.py
+++ myportfolio/myportfolio/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for myportfolio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Add your project directory to the Python path
+project_home = '/home/maricarpordan/personal_portfolio/myportfolio'
+if project_home not in sys.path:
+    sys.path.append(project_home)
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myportfolio.settings'
+
+# Activate your virtual environment
+activate_this = '/home/maricarpordan/.virtualenvs/myportfolio-venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Get WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
-
 application = get_wsgi_application()
